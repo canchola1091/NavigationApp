f922ced99f7ce5690679e9d87c1b317ef8c723fd
Eduardo Canchola <canchola700@gmail.com>
2024-04-25 15:30:47 -0600

    estilizacion statusbar

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -7,10 +7,18 @@ const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{
+      headerShown: true,
+      headerStyle: {
+        elevation: 0,
+        shadowColor: 'transparent'
+      }
+    }}>
+
       <Stack.Screen name="Home" component={ HomeScreen } />
       <Stack.Screen name="Products" component={ ProductsScreen } />
       <Stack.Screen name="Settings" component={ SettingsScreen } />
+      
     </Stack.Navigator>
   );
 }
