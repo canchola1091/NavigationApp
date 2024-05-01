d1b837875fd686b51f846d24e7023ac714f52b57
Eduardo Canchola <canchola700@gmail.com>
2024-05-01 12:08:39 -0600

    Instalacion de iconos, TopTabBars

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,11 +1,35 @@
 
+import { View, Text} from "react-native"
+import { HamburgerMenu } from "../../components/shared/HamburgerMenu";
+import Icon from 'react-native-vector-icons/Ionicons';
 
-import { View, Text } from "react-native"
 
 export const Tab1Screen = () => {
+
+  // const navigation = useNavigation();
+
+  // useEffect(() => {
+  //   navigation.setOptions({
+  //     headerLeft: () => (
+  //       <Pressable
+  //         onPress={ () => navigation.dispatch( DrawerActions.toggleDrawer ) }
+  //       >
+  //         <Text>Menú</Text>
+  //       </Pressable>
+  //     )
+  //   })
+  // }, []);
+  
+
   return (
     <View>
+
+      <HamburgerMenu/>
+
       <Text>Tab1Screen</Text>
+
+      <Icon name="rocket-outline" size={30} />
+
     </View>
   )
 }
